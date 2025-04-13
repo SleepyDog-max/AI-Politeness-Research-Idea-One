1bf14be01d6d44299f91de9345715dd044751b8b
SleepyDog-max <forward8han@gmail.com>
2025-04-13 18:00:39 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import AIPolitenessPlanner from "./planner.js";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<AIPolitenessPlanner />);
